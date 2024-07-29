75186b6de44170b0e5fa76d0600ec71beca5191b
Reinhardt Botha <botha.wr@gmail.com>
2024-07-28 22:32:36 -0700

    feat: team URLs

--- src/team.jsx
+++ src/team.jsx
@@ -7,63 +7,61 @@ import Reinhardt from "./assets/images/headshots/rein.png";
 import Victor from "./assets/images/headshots/victor.png";
 import Wonnyo from "./assets/images/headshots/wonnyo.png";
 
-// TODO: update linkedin and website links
-
 const team = [
   {
     name: "Pooja Chauhan",
     role: "Designer",
-    linkedin: "https://www.linkedin.com/in/pooja-agarwal-1b7b1b1b1/",
-    website: "https://poojaagarwal.com/",
+    linkedin: "https://www.linkedin.com/in/poojachauhan21",
+    website: "https://poojach.com",
     img: Pooja,
   },
   {
     name: "Jiali Cai",
     role: "Designer",
-    linkedin: "https://www.linkedin.com/in/pooja-agarwal-1b7b1b1b1/",
-    website: "https://poojaagarwal.com/",
+    linkedin: "https://www.linkedin.com/in/jialicjl",
+    website: "https://jia-li.com",
     img: Jiali,
   },
   {
     name: "Anderson Marques",
     role: "Designer",
-    linkedin: "https://www.linkedin.com/in/pooja-agarwal-1b7b1b1b1/",
-    website: "https://poojaagarwal.com/",
+    linkedin: "https://www.linkedin.com/in/andersonmarquesoli",
+    website: "https://andersonmarquesoliveira.com",
     img: Anderson,
   },
   {
     name: "Cheuk Yan Li",
     role: "Designer",
-    linkedin: "https://www.linkedin.com/in/pooja-agarwal-1b7b1b1b1/",
-    website: "https://poojaagarwal.com/",
+    linkedin: "https://www.linkedin.com/in/adrian-cheukyan-li",
+    website: "https://adriancyli.com",
     img: Adrian,
   },
   {
     name: "Wonnyo Hamester",
     role: "Developer",
-    linkedin: "https://www.linkedin.com/in/pooja-agarwal-1b7b1b1b1/",
-    website: "https://poojaagarwal.com/",
+    linkedin: "https://www.linkedin.com/in/wonnyo",
+    website: "https://github.com/whamester",
     img: Wonnyo,
   },
   {
     name: "Reinhardt Botha",
     role: "Developer",
-    linkedin: "https://www.linkedin.com/in/pooja-agarwal-1b7b1b1b1/",
-    website: "https://poojaagarwal.com/",
+    linkedin: "https://www.linkedin.com/in/wrbotha",
+    website: "https://github.com/ReinhardtBotha",
     img: Reinhardt,
   },
   {
     name: "Bethleen Baral",
     role: "Developer",
-    linkedin: "https://www.linkedin.com/in/pooja-agarwal-1b7b1b1b1/",
-    website: "https://poojaagarwal.com/",
+    linkedin: "https://www.linkedin.com/in/bethleen-baral",
+    website: "https://github.com/zeckkimon",
     img: Beck,
   },
   {
     name: "Victor Portus",
     role: "Developer",
-    linkedin: "https://www.linkedin.com/in/pooja-agarwal-1b7b1b1b1/",
-    website: "https://poojaagarwal.com/",
+    linkedin: "https://www.linkedin.com/in/victor-portus",
+    website: "https://github.com/Victorportus",
     img: Victor,
   },
 ];
